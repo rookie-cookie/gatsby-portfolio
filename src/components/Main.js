57030856bf13d6a326902666fd9e57dba62be48b
Jona Reyes <jonamreyes@gmail.com>
2022-01-20 13:38:48 -0500

    update

--- src/components/Main.js
+++ src/components/Main.js
@@ -180,7 +180,7 @@ class Main extends React.Component {
             Prior to working in the software development industry, I've worked in the IT industry where I've been able to develop strong analytical and troubleshooting skills. I have a passion for problem-solving and enjoy being in dynamic teams. My goal is to combine my analytical thinking with my dedication for creating beautiful, accessible web applications.
           </p>
 
-          <p>CORE TECHNOLOGIES: <br/> HTML | CSS | Sass | Javascript | jQuery | React | Redux | Node | Express | MongoDB | Git | Bootstrap | Gatsby | Heroku | Figma | VScode </p>
+          <p>CORE TECHNOLOGIES: <br/> HTML | CSS | Sass | Javascript | jQuery | React | Redux | Node | Express | MongoDB | Git | Bootstrap | Material UI | Gatsby | Heroku | Figma | VScode </p>
           <p> </p>
 
           <p>FAMILIAR WITH: <br/> Django | Linux | Docker | Kubernetes | AWS | Google Analytics </p>
